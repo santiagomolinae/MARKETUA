f7a3d15a8dc8e810f0cd6912bb4bdf0372da1496
santiagomolinae <santiago.molinae@udea.edu.co>
2019-09-14 02:22:12 -0500

    mock

--- src/app/services/product/product.service.ts
+++ src/app/services/product/product.service.ts
@@ -9,12 +9,12 @@ export class ProductService {
   constructor(private http: HttpClient) { }
 
   findProducts(searchText: string) {
-     //return this.http.get<any>(`https://my-json-server.typicode.com/ebedoyaalzate/fakeProducts/db`, {}); // Api mock
-     return this.http.get<any>(`http://marketua-develop-api.herokuapp.com/search?q=${searchText}`, {}); // Api Ruby
+     return this.http.get<any>(`https://my-json-server.typicode.com/ebedoyaalzate/fakeProducts/db`, {}); // Api mock
+     //return this.http.get<any>(`http://marketua-develop-api.herokuapp.com/search?q=${searchText}`, {}); // Api Ruby
   }
 
   productDetail(id) {
-    //return this.http.get<any>(`https://my-json-server.typicode.com/ebedoyaalzate/fakeProducts/products/${id}`, {}); // Api mock
-    return this.http.get<any>(`http://marketua-develop-api.herokuapp.com/items/${id}`, {}); // Api Ruby
+    return this.http.get<any>(`https://my-json-server.typicode.com/ebedoyaalzate/fakeProducts/products/${id}`, {}); // Api mock
+    //return this.http.get<any>(`http://marketua-develop-api.herokuapp.com/items/${id}`, {}); // Api Ruby
   }
 }
